5380a631e5f457e52941ce0dc71def69544031a6
Volodymyr Lipunov <volo_lipu@yahoo.com>
2017-06-28 21:40:43 -0700

    added geolocation support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -9,5 +9,17 @@ describe('generateMessage', () => {
     const message = generateMessage(from ,text);
     expect(message).toInclude({ from, text });
     expect(message.createdAt).toBeA('number');
+  });
+});
+
+describe('generate location message', () => {
+  it('should generate curret location message', () => {
+    const lat = 37.7726402;
+    const long = -122.40991539999997;
+    const from = 'Vovka Markovka';
+    const locationMessage = generateLocationMessage(from, lat, long);
+    expect(locationMessage.from).toBe(from);
+    expect(locationMessage.createdAt).toBeA('number');
+    expect(locationMessage.url).toBe(`https://www.google.com/maps?q=${lat},${long}`);
   });
 });
