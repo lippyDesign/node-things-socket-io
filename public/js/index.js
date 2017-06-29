822d7ddafe4fd8703dc39eaa417a9c031271cf46
Volodymyr Lipunov <volo_lipu@yahoo.com>
2017-06-28 23:45:00 -0700

    added css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,25 +26,30 @@ socket.on('newLocationMessage', function(message) {
 
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
-
+  var messageTextbox = $('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextbox.val()
   }, function() {
-
+    messageTextbox.val('')
   });
 })
 
 const locationButton = $('#send-location');
+
 locationButton.on('click', function() {
   if (!navigator.geolocation) return alert('Geolocation not supported by your browser');
+
+  locationButton.attr('disabled', 'disabled').text('Sending Location...');
 
   navigator.geolocation.getCurrentPosition(function(position) {
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
+    locationButton.removeAttr('disabled').text('Send Location');
   }, function() {
     alert('Unable to fetch location');
+    locationButton.removeAttr('disabled').text('Send Location');
   });
 })
